716d9e5dc712f940e471a3401efe52cfbf71ae9b
Vany Z <ivanzet@i.ua>
2019-11-24 15:53:25 +0200

    add side-bar

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
-    
+    let $sideBar = $('#side-bar'),
+    	$helpProc = $('#h-d-4');
+
+	$helpProc.click(function () {
+		$sideBar.addClass('bounceInLeft').css({
+			opacity:1
+		});
+	});
 });
