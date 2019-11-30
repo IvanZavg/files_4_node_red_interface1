e435bf2e0bfdd8077d144f64899636e587ab0b0d
Vany Z <ivanzet@i.ua>
2019-11-30 20:14:46 +0200

    change atibutes, ad some logick

--- main.js
+++ main.js
@@ -1,20 +1,81 @@
 $( document ).ready(function() {
+
     let $sideBar = $('#side-bar'),
-    	$processes = $('.hd-titels'),
-    	sideBarIsOpen = 'N';
+    	$headerTitels = $('.main-menu-titles'),
+    	$mBlockTitle = $('#main-block-title'),
+    	$cntr4OutPut = $('#main-block-output');
 
-	$processes.click(()=>{	
-		let that = $(this);
+   	let sideBarIsOpen = 'N';
+	let mainMenuData = {
+		incomProc : {
+			id : "incom-proc",
+			infoTitle : "",
+			infoBlock : "",
+			isOpen : false,
+			subTitels : []
+		},
+		motivTalks : {
+			id : "motiv-proc",
+			isOpen : false,
+			infoTitle : "",
+			infoBlock : "",
+			subTitels : []
+		},
+		afterTestTalks :{
+			id : "after-test-talks",
+			isOpen : false,
+			infoTitle : "",
+			infoBlock : "",
+			subTitels : []
+		},
+		helpProc : {
+			id : "help-proc",
+			isOpen : false,
+			infoTitle : "",
+			infoBlock : "",
+			subTitels : []
+		}
+	};   
 
+
+	$headerTitels.click(function(){	
+
+		let $that = $(this);
+		let menuName = $that.attr('name');
+		let menuData = mainMenuData[menuName];
+
+		viewSideBar(menuData);
+		
+	});
+
+
+
+	function viewSideBar(mName){
 		if(sideBarIsOpen == 'N'){
+			mName.isOpen = true;
 			togleSideBar();
 			sideBarIsOpen = 'Y';
-		}else{
+		}
+		else if (sideBarIsOpen == 'Y' && mName.isOpen == true){
+			sideBarIsOpen = 'N';
+			mName.isOpen == false;
+			$sideBar.removeClass('bounceInLeft').addClass('bounceOutLeft');
+		}
+		else{
+			tglMainMenuState();
+			mName.isOpen = true;
 			togleSideBar();
 			setTimeout(togleSideBar, 500);
 		}
-		
-	});
+	}
+
+	function tglMainMenuState(){
+		for(key in mainMenuData){
+			if(mainMenuData[key].isOpen == true){
+				mainMenuData[key].isOpen = false;
+			}
+		}
+	}
 	
 	function togleSideBar () {
 
